f3a2bbbab1f104d2b22f386254fd9eb4ea3cdb8a
Eli <eli@hanokh.com.br>
2023-08-21 02:12:36 -0300

    Adicionado controlador do server.

---
+++ controller/http/server_controller.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+# coding: utf-8
+from model.http.server import Server
+
+
+class ServerController:
+    def __init__(self, host_ip, port, app):
+        self.host_ip = host_ip
+        self.port = port
+        self.app = app
+        self.server = Server()
+        self.server.set_host_ip(self.host_ip)
+        self.server.set_port(self.port)
+        self.server.set_app(self.app)
+        self.server.run_server()
